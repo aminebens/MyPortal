22c21ae9663613c622d8fb9c648da5ae048ec931
yaantech <yanismedjkoune@gmail.com>
2016-07-13 14:49:21 -0400

    rien

---
+++ myPortal/src/main/java/ca/isimtl/myPortal/dao/MessageDao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.isimtl.myPortal.dao;
+
+import ca.isimtl.myPortal.model.Message;
+import ca.isimtl.myPortal.model.Sujet;
+import java.util.List;
+
+/**
+ *
+ * @author yaantech
+ */
+public interface MessageDao {
+    Message findById(int id);
+    void saveMessage(Message msg);
+    List<Message> getMessageSujet(Sujet sujet);
+}
